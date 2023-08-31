012212b10707da766db04aaf125dc7dd2426f389
Yohan Prakoso <prakoso.yohan@gmail.com>
2023-08-31 15:05:30 +0800

    Add feature to project: 1. add printf feature 2. enable printf float

--- yRosSerial_Development/Core/Src/main.c
+++ yRosSerial_Development/Core/Src/main.c
@@ -264,6 +264,17 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
 {
 	yRosSerial_handleCompleteReceive(huart, Size);
 }
+
+int _write(int file, char *ptr, int len)
+{
+	int DataIdx;
+
+	for (DataIdx = 0; DataIdx < len; DataIdx++)
+	{
+		ITM_SendChar(*ptr++);
+	}
+	return len;
+}
 /* USER CODE END 4 */
 
 /**
